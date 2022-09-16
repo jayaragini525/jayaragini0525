69f4459189a9f923d6fe75e4f0096da95812c497
jayaragini525 <113323348+jayaragini525@users.noreply.github.com>
2022-09-16 09:34:46 +0530

    Add files via upload

---
+++ virtual arithmetic.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class arithmetic
+{
+	int x,y ,add,sub,Multi,divi,module;
+	public:
+		virtual void  display()
+		{
+		    x&y;
+		}
+};
+class add:public arithmetic
+{
+	int x,y;
+	public:
+	void display()
+	{
+		cout<<"add is"<<x+y<<endl;
+	}
+};
+class sub:public arithmetic
+{
+	int x,y;
+	public:
+	void display()
+		{
+		cout<<"sub is  "<<x-y<<endl;
+	}
+};
+class Multi:public arithmetic
+{
+	int x,y;
+	public:
+	void display()
+	{
+		cout<<"Multi is  "<<x*y<<endl;
+	}
+};
+class divi: public arithmetic
+{
+	int x,y;
+	public:
+	void display()
+	{
+		cout<<"divi is "<<x/y<<endl;
+		}
+};
+class module: public arithmetic
+{
+	int x,y;
+	public:
+	void display()
+	{
+		cout<<"module is "<<x%y<<endl;
+	}
+};
+int main()
+{
+	arithmetic *a;
+	add a1;
+	sub s1;
+	Multi m1;
+	divi d1;
+	module m2;
+	a=&a1;
+	a->display();
+	a=&s1;
+	a->display();
+	a=&m1;
+	a->display();
+	a=&d1;
+	a->display();
+	a=&m2;
+	a->display();
+}
+	
